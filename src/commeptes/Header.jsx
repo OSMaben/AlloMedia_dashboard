aa7e2207acf69abd017582909313cb04a4e0f64c
OSMaben <freegiftcardishere@gmail.com>
2024-10-25 15:10:52 +0100

    fixing the problem of the design  of the  header it was showing different design button for each page

--- src/commeptes/Header.jsx
+++ src/commeptes/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { Deconxion } from "../redux/features/authSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { motion } from "framer-motion";
@@ -13,6 +13,7 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import { XMarkIcon, Bars3Icon } from "@heroicons/react/24/solid";
+import styled, { createGlobalStyle } from 'styled-components';
 
 const NAV_MENU = [
   {
@@ -55,7 +56,165 @@ const Header = () => {
     setMenuOpen((cur) => !cur);
   };
 
+
+  const GlobalStyles = createGlobalStyle`
+
+    .navbar {
+      padding: 2rem 0;
+    }
+    .navbar .row {
+      justify-content: space-between;
+    }
+    .navbar .logo {
+      justify-content: center;
+    }
+    .navbar .nav-list a {
+      display: inline-block;
+      padding: 1rem 0.5rem 1rem;
+    }
+    .navbar .nav-list a:not(:last-child) {
+      margin-right: 0.5rem;
+    }
+    .navbar .col form {
+      background-color: var(--white-color);
+      border-radius: 0.5rem;
+      box-shadow: var(--box-shadow);
+      border: 1px solid rgba(105, 105, 123, 0.1);
+      padding: 0.5rem 0;
+      margin-right: 1.5rem;
+      width: 25rem;
+      position: relative;
+    }
+    .navbar .col form input {
+      text-indent: 1rem;
+      font-size: 1.4rem;
+      width: 100%;
+    }
+    .navbar .col form input::placeholder {
+      color: var(--default-color);
+    }
+    .navbar .col form button {
+      justify-content: center;
+      padding: 0.5rem;
+      background-color: transparent;
+      border-left: 1px solid rgba(105, 105, 123, 0.1);
+      font-size: 1.7rem;
+      color: var(--default-color);
+      position: absolute;
+      top: 50%;
+      right: 1rem;
+      transform: translateY(-50%);
+    }
+    .navbar .cart-icon {
+      justify-content: center;
+      color: var(--default-color);
+      font-size: 2rem;
+      margin-right: 1.5rem;
+      position: relative;
+    }
+    .navbar .cart-icon span {
+      position: absolute;
+      top: -1.3rem;
+      right: -1rem;
+      padding: 0.1rem 0.5rem;
+      color: var(--white-color);
+      background-color: var(--primary-color);
+      border-radius: 50%;
+      font-size: 1.2rem;
+      font-weight: 600;
+    }
+    
+    .btn {
+      display: inline-block;
+      background-color: var(--primary-color);
+      color: var(--white-color);
+      padding: 0.7rem 2.5rem;
+      border-radius: 1rem;
+      font-size: inherit;
+    }
+    
+    .navbar .btn {
+      margin-left: 1rem;
+      cursor: pointer;
+    }
+    
+    .hamburger {
+      font-size: 2.5rem;
+      background-color: var(--primary-color);
+      border-radius: 0.3rem;
+      color: var(--white-color);
+      padding: 0.5rem 0.7rem;
+      justify-content: center;
+      cursor: pointer;
+      display: none;
+    }
+    
+    .nav-list .close {
+      display: none;
+    }
+    
+    @media (max-width: 1300px) {
+      .navbar .col form {
+        display: none;
+      }
+    }
+    @media (max-width: 800px) {
+      .navbar .col {
+        display: none;
+      }
+    
+      .navbar .nav-list {
+        position: fixed;
+        top: 0;
+        left: -100%;
+        width: 100%;
+        max-width: 35rem;
+        height: 100%;
+        background-color: var(--white-color);
+        z-index: 999;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 5rem 2rem;
+        transition: left 300ms linear;
+      }
+    
+      .hamburger {
+        display: flex;
+      }
+    
+      .nav-list a {
+        text-transform: uppercase;
+        font-weight: 600;
+      }
+    
+      .nav-list a:not(:last-child) {
+        margin: 0 0 1rem 0;
+      }
+    
+      .nav-list .close {
+        position: absolute;
+        top: 2rem;
+        right: 2rem;
+        font-size: 3rem;
+        cursor: pointer;
+        padding: 0.5rem 0.7rem;
+        display: flex;
+        justify-content: center;
+      }
+    
+      .nav-list.show {
+        left: 0;
+      }
+    
+      .navbar .logo img {
+        width: 15rem;
+      }
+    }
+  `
+
   return (
+    <>
+    <GlobalStyles/>
     <div className="px-10 sticky top-4 z-50">
       <div className="mx-auto container">
         <MTNavbar
@@ -64,12 +223,12 @@ const Header = () => {
           className="z-50 mt-6 relative border-0 pr-3 py-3 pl-6"
         >
           <div className="flex items-center justify-between">
-            <Typography
+            <Link to={'/'}
               color="blue-gray"
               className="text-3xl font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
             >
               AlloMedia
-            </Typography>
+            </Link >
             <ul className="ml-10 hidden items-center gap-8 lg:flex">
               {!isLogin ? (
                 <>
@@ -92,7 +251,7 @@ const Header = () => {
                 <>
                   {/* <NavLink
                     to="/profile"
-                    className="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
+                    className="text-2xl font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
                   >
                     Profile
                   </NavLink> */}
@@ -101,7 +260,7 @@ const Header = () => {
                   {user.user.role === "client" && (
                     <NavLink
                       to="/profile"
-                      className="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
+                      className="text-2xl font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
                     >
                       Profile
                     </NavLink>
@@ -109,7 +268,7 @@ const Header = () => {
                   {user.user.role === "livreur" && (
                     <NavLink
                       to="/livreur"
-                      className="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
+                      className="text-2xl font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
                     >
                        Dashboard
                     </NavLink>
@@ -117,7 +276,7 @@ const Header = () => {
                   {user.user.role === "manager" && (
                     <NavLink
                       to="/manager-dashboard"
-                      className="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
+                      className="text-2xl font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
                     >
                        Dashboard
                     </NavLink>
@@ -125,13 +284,13 @@ const Header = () => {
                   {user.user.role === "admin" && (
                     <NavLink
                       to="/dashboard"
-                      className="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
+                      className="text-2xl font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
                     >
                        Dashboard
                     </NavLink>
                   )}
 
-                  <Button variant="text" onClick={logout}>
+                  <Button variant="text" className="text-2xl" onClick={logout}>
                     Log out
                   </Button>
                 </>
@@ -157,14 +316,14 @@ const Header = () => {
                   <>
                     <NavLink
                       to="/signin"
-                      className="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
+                      className="text-2xl font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
                       aria-label="Sign in"
                     >
                       Sign in
                     </NavLink>
                     <NavLink
                       to="/signup"
-                      className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+                      className="inline-flex items-center justify-center px-4 py-2 text-2xl font-medium leading-6 text-white whitespace-no-wrap bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
                       aria-label="Sign up"
                     >
                       Sign up
@@ -191,6 +350,8 @@ const Header = () => {
         </MTNavbar>
       </div>
     </div>
+    </>
+
   );
 };
 
--- src/commeptes/homeComponents/aboutUs.jsx
+++ src/commeptes/homeComponents/aboutUs.jsx
@@ -1,34 +1,34 @@
 export default function AboutUs() {
     return (
         <>
-            <section class="section about" id="about">
-                <div class="row container">
-                    <div class="col">
+            <section className="section about" id="about">
+                <div className="row container">
+                    <div className="col">
                         <img src="./images/delivery-guy-2.svg" alt="" />
                     </div>
-                    <div class="col">
+                    <div className="col">
                         <h2>Take a look at the benefits we offer for you</h2>
                         <p>
                             Good service means a friendly, welcoming service. A restaurant owner
                             should not merely strive to avoid bad service,
                         </p>
-                        <div class="d-grid">
-                            <div class="card">
+                        <div className="d-grid">
+                            <div className="card">
                                 <img src="./images/car-icon.svg" alt="" />
                                 <h4>Free Home Delivary</h4>
                                 <span>For all orders over $50</span>
                             </div>
-                            <div class="card">
+                            <div className="card">
                                 <img src="./images/dollar-icon.svg" alt="" />
                                 <h4>Return & Refund</h4>
                                 <span>Money Back Guarantee</span>
                             </div>
-                            <div class="card">
+                            <div className="card">
                                 <img src="./images/security-icon.svg" alt="" />
                                 <h4>Secure Payment</h4>
                                 <span>100% Secure Payment</span>
                             </div>
-                            <div class="card">
+                            <div className="card">
                                 <img src="./images/time-icon.svg" alt="" />
                                 <h4>Quality Support</h4>
                                 <span>Alway Online 24/7</span>
